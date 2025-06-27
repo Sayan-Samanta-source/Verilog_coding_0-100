5fc1534829cb6662d4c30d2afc85f9341b72a799
Sayan Samanta <samantasayan773@gmail.com>
2025-06-27 09:27:50 +0530

    Add files via upload

---
+++ full_subtractor.v
@@ -0,0 +1,27 @@
+///// full subtractor////
+module full_subtractor (a,b,cin,sub,borrow);
+input wire a;
+input wire b;
+input wire cin;
+output wire sub;
+output wire borrow;
+assign sub = a ^ b ^ cin;
+assign borrow = (~a & b) | ((~(a ^ b)) & cin);
+endmodule
+////test bench////
+module tb_full_subtractor;
+reg a, b, cin;
+wire sub, borrow;
+full_subtractor s1 (a,b,cin,sub,borrow);
+initial 
+begin
+a = 0; b = 0; cin = 0; #100;
+a = 0; b = 0; cin = 1; #100;
+a = 0; b = 1; cin = 0; #100;
+a = 0; b = 1; cin = 1; #100;
+a = 1; b = 0; cin = 0; #100;
+a = 1; b = 0; cin = 1; #100;
+a = 1; b = 1; cin = 0; #100;
+a = 1; b = 1; cin = 1; 
+end
+endmodule
